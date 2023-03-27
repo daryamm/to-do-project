b4c86c56628a6b9d511f1e3d547a1746da761c06
daryamm <dash9696@list.ru>
2023-03-27 18:45:47 +0300

    fix: add input

--- src/app.js
+++ src/app.js
@@ -58,6 +58,17 @@ export function App() {
               className='h-4 w-4 rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0'
               checked
             />
+            <span className='ml-4 text-sm font-medium text-gray-400'> Сверстать макет из Figma </span>
+          </label>
+        </div>
+        <div>
+          <label className='m-5 mb-4 mt-6 flex items-center rounded border border-gray-300 py-4 px-3'>
+            <input
+              type='checkbox'
+              // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
+              className='h-4 w-4 rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0'
+              checked
+            />
             <span className='ml-4 text-sm font-medium text-gray-400'>Добавить Tailwind CSS в проект</span>
           </label>
         </div>
